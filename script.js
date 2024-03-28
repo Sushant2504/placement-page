6e3f6613d776aeec266a9f2203e4cceab610462b
Sushant <sushantnadavade006@gmail.com>
2024-03-28 21:42:45 +0530

    all files added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Show the loading spinner
+document.querySelector('.loader-wrapper').style.display = 'block';
+
+// Simulate loading time (replace this with your actual logic)
+setTimeout(() => {
+  // Hide the loading spinner
+  document.querySelector('.loader-wrapper').style.display = 'none';
+}, 2000);  // Delay 2 seconds for demonstration
